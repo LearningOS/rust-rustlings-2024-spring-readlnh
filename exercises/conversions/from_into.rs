998a045241033abdde17b8e63ebc9966947cd7d5
readlnh <readlnh@163.com>
2024-04-11 05:30:35 +0200

    finish conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let res: Vec<&str> = s.split(",").collect();
+        if res.len() != 2 {
+            return Person::default();
+        }
+        if res[0].len() == 0 {
+            return Person::default();
+        }
+        match res[1].parse::<usize>() {
+            Err(_) => {
+                return Person::default();
+            }
+            Ok(age_num) => {
+                return Person {
+                    name: res[0].to_string(),
+                    age: age_num,
+                }
+            }
+        }
     }
 }
 
